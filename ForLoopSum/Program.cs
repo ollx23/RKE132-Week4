8973d6b286cf43e48bdec1e43cf8f25982fa8e67
Balja. <gohot.poolggmail.com.>
2024-04-27 16:49:45 +0300

    add wee 4 code

---
+++ ForLoopSum/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+
+int sum = 0;
+
+
+for(int i = 0; i < 11; i++)
+{
+    Console.WriteLine($"i = {i}");
+    Console.WriteLine($"Current total: {sum}");
+    sum = sum + i;
+}
+
+Console.WriteLine($"Fianl total {sum}");
